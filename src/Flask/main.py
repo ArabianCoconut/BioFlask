602e65efc270ac6ac2a4d568dc85b3e45bb9f582
ArabianCoconut <16560968+ArabianCoconut@users.noreply.github.com>
2023-05-05 15:14:55 +0530

    added docstring.

--- src/Flask/main.py
+++ src/Flask/main.py
@@ -1,25 +1,25 @@
-"""
+'''
 * @Author: ArabianCoconut
 * @Version: 0.0.1 Alpha
 * @Description: This is the main application file for the flask application
-"""
+'''
 import os
-import Bioprocess as Bio
 from flask import Flask, render_template, request, redirect, url_for
 from flask_cors import CORS
+import Bioprocess as Bio
 
 app = Flask(__name__)
 CORS(app)
 
 
 def start_app(host, port, debug=bool()) -> Flask:
-    """
+    '''
     * This is the main function/config for the application
     * @param {string} host
     * @param {string} port
     * @param {bool} debug
     * @return {Flask} app
-    """
+    '''
     Bio.qr_code(hostname=host, port=port)
 
     if bool(debug) is True:
@@ -32,44 +32,53 @@ def start_app(host, port, debug=bool()) -> Flask:
 
 @app.route("/", methods=['GET'])
 def html():
-    """ Renders the html page """
+    ''' Renders the html page '''
     return render_template('index.html')
 
 
 @app.route("/upload", methods=['POST', 'GET'])  # type: ignore
 def upload():
-    """
+    '''
     * This is the route for the upload page
     * @param {POST} request
     * @param {GET} request
     * @return {JSON} status
-    """
+    '''
     if request.method == 'POST':
-        data = request.data.decode('utf-8')
+        data: str = request.data.decode('utf-8')
         Bio.load_json(data)
         return redirect(url_for('uploaded', _method='GET'))
 
 
 @app.route("/api/uploaded", methods=['GET'])
 def uploaded():
-    """
+    '''
     * This is the route for the uploaded page
-    """
+    '''
     return render_template('upload.html')
 
 
 @app.route("/result", methods=['GET'])
 def results():
+    '''
+    * This is the route for the results page
+    '''
     return render_template('results.html')
 
 
 @app.route("/api/results", methods=['GET'])
 def results_api():
+    '''
+    * This is the route for the results page
+    '''
     return redirect(url_for('static', filename='results.txt'))
 
 
 @app.route("/api/delete", methods=['POST', 'GET'])
 def delete():
+    '''
+    * This is the route for the delete page
+    '''
     os.remove("static/results.txt")
     os.remove("static/qr.png")
     return redirect(url_for('html', _method='GET'))
